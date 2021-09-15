98831b4ff33647b2aa135bf4cb7a0a03a1db547b
alecan27 <53607663+alecan27@users.noreply.github.com>
2021-09-15 22:20:24 +0300

    hi

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * test - tests if wildcard is a proper match
+ * @s5: string to compare
+ * @s6: wildcard match to compare
+ * @t5: first spot that was checked
+ * @t6: first spot in wildcard comparison
+ *
+ * Return: spot that matches or NULL
+ */
 char *test(char *s5, char *s6, char *t5, char *t6)
 {
 	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
@@ -9,7 +18,13 @@ char *test(char *s5, char *s6, char *t5, char *t6)
 	else
 		return (test(s5 + 1, s6 + 1, t5, t6));
 }
-
+/**
+ * wildcard - checks for the wildcards
+ * @s3: string to compare to
+ * @s4: string to check
+ *
+ * Return: pointer to spot in s3 that matches s4
+ */
 char *wildcard(char *s3, char *s4)
 {
 	if (*s3 != *s4)
@@ -21,7 +36,13 @@ char *wildcard(char *s3, char *s4)
 	return (test(s3 + 1, s4 + 1, s3, s4));
 }
 
-
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string, may contain wildcard *
+ *
+ * Return: 1 if match, 0 if not
+ */
 int wildcmp(char *s1, char *s2)
 {
 	char *p;
